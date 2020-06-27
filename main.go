d59b05d38bc4377a6a7c1d2dd78fd6951387699b
zhaoyi <jackfield1611@gmail.com>
2020-06-27 21:16:27 +0000

    add gce and cloud vision

--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"reflect"
+	"strconv"
+
+	"cloud.google.com/go/storage"
+	"github.com/gorilla/mux"
+	"github.com/olivere/elastic"
+	"github.com/pborman/uuid"
+)
+
+var (
+	mediaTypes = map[string]string{
+		".jpeg": "image",
+		".jpg":  "image",
+		".gif":  "image",
+		".png":  "image",
+		".mov":  "video",
+		".mp4":  "video",
+		".avi":  "video",
+		".flv":  "video",
+		".wmv":  "video",
+	}
+)
+
+const (
+	POST_INDEX  = "post"
+	DISTANCE    = "200km"
+	BUCKET_NAME = "cloudzen-bucket"
 )
 
 type Location struct {
@@ -19,23 +49,31 @@ type Post struct {
 	User     string   `json:"user"`
 	Message  string   `json:"message"`
 	Location Location `json:"location"`
-	Url      string   `json:"url"`
+	Url      string   `json:"url"` // From GCS
 	Type     string   `json:"type"`
-	Face     float32  `json:"face"`
+	Face     float32  `json:"face"` // From Cloud Vision
 }
 
 type Statics struct {
-	PORT string `json:"PORT"`
+	ES_URL string `json:"ES_URL"`
+	PORT   string `json:"PORT"`
 }
+
+var ES_URL, PORT string
 
 func main() {
-	var PORT = readPort()
+	ES_URL, PORT = readStatics()
 	fmt.Println("started-service")
-	http.HandleFunc("/post", handlerPost)
-	log.Fatal(http.ListenAndServe(PORT, nil))
+	r := mux.NewRouter()
+
+	r.Handle("/post", http.HandlerFunc(handlerPost)).Methods("POST", "OPTIONS")
+	r.Handle("/search", http.HandlerFunc(handlerSearch)).Methods("GET", "OPTIONS")
+	r.Handle("/cluster", http.HandlerFunc(handlerCluster)).Methods("GET", "OPTIONS")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-func readPort() string {
+func readStatics() (string, string) {
 	jsonFile, err := os.Open("statics.json")
 
 	if err != nil {
@@ -47,17 +85,231 @@ func readPort() string {
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 	var statics Statics
 	json.Unmarshal(byteValue, &statics)
-	return statics.PORT
+	return statics.ES_URL, statics.PORT
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
 	// Parse from body of request to get a json object.
 	fmt.Println("Received one post request")
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
-		panic(err)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	if r.Method == "OPTIONS" {
+		return
 	}
 
-	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	// 1. Read parameters from client
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+
+	p := &Post{
+		User:    r.FormValue("user"),
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+
+	// 2. Save to GCS
+	// 2.1 Get the file, header, url from the req
+	file, header, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusBadRequest)
+		fmt.Printf("Image is not available %v\n", err)
+		return
+	}
+
+	suffix := filepath.Ext(header.Filename)
+	if t, ok := mediaTypes[suffix]; ok {
+		p.Type = t
+	} else {
+		p.Type = "unknown"
+	}
+
+	id := uuid.New()
+	mediaLink, err := saveToGCS(file, id)
+	if err != nil {
+		http.Error(w, "Failed to save image to GCS", http.StatusInternalServerError)
+		fmt.Printf("Failed to save image to GCS %v\n", err)
+		return
+	}
+	p.Url = mediaLink
+
+	// 3. Annotate w/ Cloud Vision
+	if p.Type == "image" {
+		uri := fmt.Sprintf("gs://%s/%s", BUCKET_NAME, id)
+		if score, err := annotate(uri); err != nil {
+			http.Error(w, "Failed to annotate image", http.StatusInternalServerError)
+			fmt.Printf("Failed to annotate the image %v\n", err)
+			return
+		} else {
+			p.Face = score
+		}
+	}
+
+	// 4. Save to ES
+	err = saveToES(p, POST_INDEX, id)
+	if err != nil {
+		http.Error(w, "Failed to save post to Elasticsearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to save post to Elasticsearch %v\n", err)
+		return
+	}
+	fmt.Printf("Post saved to Elasticsearch\n")
+}
+
+func readFromES(query elastic.Query, index string) (*elastic.SearchResult, error) {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL))
+	if err != nil {
+		return nil, err
+	}
+
+	searchResult, err := client.Search().
+		Index(index).
+		Query(query).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return searchResult, nil
+}
+
+func getPostFromSearchResult(searchResult *elastic.SearchResult) []Post {
+	var ptype Post
+	var posts []Post
+
+	for _, item := range searchResult.Each(reflect.TypeOf(ptype)) {
+		p := item.(Post)
+		posts = append(posts, p)
+	}
+	return posts
+}
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	if r.Method == "OPTIONS" {
+		return
+	}
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+	// range is optional
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}
+	fmt.Println("range is ", ran)
+
+	query := elastic.NewGeoDistanceQuery("location")
+	query = query.Distance(ran).Lat(lat).Lon(lon)
+	searchResult, err := readFromES(query, POST_INDEX)
+	if err != nil {
+		http.Error(w, "Failed to read post from Elasticsearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to read post from Elasticsearch %v.\n", err)
+		return
+	}
+	posts := getPostFromSearchResult(searchResult)
+
+	js, err := json.Marshal(posts)
+	if err != nil {
+		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
+		fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
+		return
+	}
+	w.Write(js)
+}
+
+func saveToGCS(r io.Reader, objectName string) (string, error) {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Error occurs: %v", err)
+		return "", err
+	}
+
+	bucket := client.Bucket(BUCKET_NAME)
+	// A quick check for the bucket existence
+	if _, err := bucket.Attrs(ctx); err != nil {
+		log.Fatalf("Error occurs: %v", err)
+		return "", err
+	}
+
+	object := bucket.Object(objectName)
+	wc := object.NewWriter(ctx)
+	if _, err := io.Copy(wc, r); err != nil {
+		return "", err
+	}
+
+	if err := wc.Close(); err != nil {
+		return "", err
+	}
+
+	if err := object.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return "", err
+	}
+
+	// Load the attribute of the saved object
+	attrs, err := object.Attrs(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("Image is saved to GCS: %s\n", attrs.MediaLink)
+	return attrs.MediaLink, nil
+}
+
+func handlerCluster(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one cluster request")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	if r.Method == "OPTIONS" {
+		return
+	}
+
+	term := r.URL.Query().Get("term")
+	query := elastic.NewRangeQuery(term).Gte(0.8)
+
+	searchResult, err := readFromES(query, POST_INDEX)
+	if err != nil {
+		http.Error(w, "Failed to read from Elasticsearch", http.StatusInternalServerError)
+		return
+	}
+
+	posts := getPostFromSearchResult(searchResult)
+	js, err := json.Marshal(posts)
+	if err != nil {
+		http.Error(w, "Failed to parse post object", http.StatusInternalServerError)
+		fmt.Printf("Failed to parse post object %v\n", err)
+		return
+	}
+	w.Write(js)
+}
+
+func saveToES(post *Post, index string, id string) error {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL))
+	if err != nil {
+		return err
+	}
+
+	_, err = client.Index().
+		Index(index).
+		Id(id).
+		BodyJson(post).
+		Do(context.Background())
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Post is saved to index: %s\n", post.Message)
+	return nil
 }
